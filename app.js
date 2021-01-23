6268bce15d915e38ad0c26620757fc5aff48cd72
kajol-rep <75063748+kajol-rep@users.noreply.github.com>
2021-01-23 19:32:40 +0530

    input, output processed
    Functionality working fine with complete styles.

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ submitBtn.addEventListener("click", btnHandler)
 
 
 function btnHandler(){
-console.log("hello")
-
 var n = luckyNum.value;
 var date=birthDate.value
 var d = 0 ;
@@ -18,22 +16,29 @@ for(var i=0;i<date.length;i++){
     if(!(i==4||i==7)){
         
         d = d + Number(date[i]);
-        console.log(d)
+        
     }
-    else{
-        console.log("no")
-    }
+    
 }
-console.log(d)
 
-    console.log(n)
     if(d%n==0){
-        console.log("done");
-        outPut.innerHTML = "its done";
+        
+        location.href = "luckyBday.html";
     }
     else{
-        console.log("not done");
-        outPut.innerHTML = "not done";
+        
+        location.href = "luckyYou.html"
     }
 
 }
+
+function bigImg(x) {
+    x.style.height = "50rem";
+    x.style.width = "50rem";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "20rem";
+    x.style.width = "20rem";
+  }
+
